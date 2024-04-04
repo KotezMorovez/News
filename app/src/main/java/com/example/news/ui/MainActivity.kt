296cd5411b0cea08d6523320ab4688c09e705cf0
KotezMorovez <odevyatlovskaya@gmail.com>
2024-04-04 19:01:10 +0500

    Refactor navigation

---
+++ app/src/main/java/com/example/news/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.news.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.news.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
