65cf309d9ae7bc003695d6b7fb64d45204c6da5b
KotezMorovez <odevyatlovskaya@gmail.com>
2024-09-30 13:34:28 +0500

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "com.example.news.ui.utils.TestRunner"
         vectorDrawables {
@@ -75,11 +75,11 @@ android {
 dependencies {
 //    Main
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.activity:activity-compose:1.9.2")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.databinding:viewbinding:8.5.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
+    implementation("androidx.databinding:viewbinding:8.6.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
 
 //    UI Tools
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
@@ -88,10 +88,10 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
 //    Dagger
-    implementation("com.google.dagger:dagger:2.46.1")
-    implementation("androidx.test:runner:1.6.0")
+    implementation("com.google.dagger:dagger:2.51.1")
+    implementation("androidx.test:runner:1.6.2")
     implementation("com.android.identity:identity-credential-android:20231002")
-    kapt("com.google.dagger:dagger-compiler:2.46.1")
+    kapt("com.google.dagger:dagger-compiler:2.51.1")
     kapt("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.2")
 
 //    Retrofit
@@ -100,33 +100,33 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
 //    Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-storage")
 
 //    Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.1")
 
 //    Date Time
     implementation("net.danlew:android.joda:2.12.7")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0-RC.2")
 
 //    Test
-    testImplementation("androidx.test:core:1.6.0")
+    testImplementation("androidx.test:core:1.6.1")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
-    debugImplementation("androidx.fragment:fragment-testing:1.8.0")
-    kaptTest("com.google.dagger:dagger-compiler:2.46.1")
+    debugImplementation("androidx.fragment:fragment-testing:1.8.3")
+    kaptTest("com.google.dagger:dagger-compiler:2.51.1")
     kaptTest("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.2")
-    kaptAndroidTest("com.google.dagger:dagger-compiler:2.46.1")
+    kaptAndroidTest("com.google.dagger:dagger-compiler:2.51.1")
     kaptAndroidTest("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.2")
 
 //      JUnit4
     testImplementation("junit:junit:4.13.2")
-    testImplementation("androidx.test.ext:junit:1.2.0")
-    androidTestImplementation("androidx.test.ext:junit:1.2.0")
+    testImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("junit:junit:4.13.2")
 
 //      Mockito
@@ -140,8 +140,8 @@ dependencies {
     testImplementation("org.robolectric:robolectric:4.12.2")
 
 //      Espresso
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
-    debugImplementation("androidx.fragment:fragment-testing:1.8.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    debugImplementation("androidx.fragment:fragment-testing:1.8.3")
     implementation("androidx.test.uiautomator:uiautomator:2.3.0")
     androidTestImplementation("com.android.support.test.uiautomator:uiautomator-v18:2.1.3")
 }
